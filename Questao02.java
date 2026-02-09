bb5f2601cb24d68e78579b2561e419776929668d
rogeriocsl <carlosrog1414@hotmail.com>
2026-02-09 19:46:02 -0300

    Add initial Java classes with basic functionality for Main, PrimeiroPrograma, and Questao02

---
+++ Questao02.java
@@ -0,0 +1,21 @@
+public class Questao02 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+
+        int soma;
+        int sub;
+        int multi;
+        double divi;
+        soma = a + b;
+        sub = a - b;
+        multi = a * b;
+        divi = a / b;
+
+        System.out.println("Soma = " + soma);
+        System.out.println("Subtração = " + sub);
+        System.out.println("Multiplicação = " + multi);
+        System.out.println("Divisão  = " + divi);
+
+    }
+}
