e0b3e1eeecc6a1de20c7195507ea981cecdb2918
rogeriocsl <carlosrog1414@hotmail.com>
2026-02-09 20:10:34 -0300

    Add Questao03 class to determine age eligibility

---
+++ Questao03.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Questao03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite Sua idade:");
+        int idade = scanner.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("Maior de idade!");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+}
